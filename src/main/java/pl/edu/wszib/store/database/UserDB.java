6ddbc175303c5348aec28361dbbd577a449ef6d5
Karolina Dyjak <karolina.dyjak5@gmail.com>
2023-01-03 18:34:57 +0100

    Changing array of users to list

--- src/main/java/pl/edu/wszib/store/database/UserDB.java
+++ src/main/java/pl/edu/wszib/store/database/UserDB.java
@@ -2,19 +2,21 @@ package pl.edu.wszib.store.database;
 
 import pl.edu.wszib.store.entity.User;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 
 public class UserDB {
-    private User[] users = new User[2];
+//    private User[] users = new User[2];
+    List<User> users = new ArrayList<>();
     private static final UserDB instance = new UserDB();
 
     private UserDB() {
-        this.users[0] = new User("admin",
-                "336284edfa1cdc721ca975e60ae6e2b0", User.Role.ADMIN);
-        this.users[1] = new User("kdyjak",
-                "d27a05b1e2e046b88a158948fc9160ba", User.Role.USER);
-
+        this.users.add(new User("admin",
+                "336284edfa1cdc721ca975e60ae6e2b0", User.Role.ADMIN));
+        this.users.add(new User("kdyjak",
+                "d27a05b1e2e046b88a158948fc9160ba", User.Role.USER));
     }
 
     public Optional <User> findByLogin(String login) {
@@ -37,15 +39,13 @@ public class UserDB {
     }
 
     public void addUser(User user){
-        User[] newUsers = new User[this.users.length + 1];
-        System.arraycopy(this.users, 0, newUsers, 0, this.users.length);
-        newUsers[newUsers.length - 1] = user;
-        this.users = newUsers;
+        this.users.add(user);
+//        this.users = newUsers;
     }
 
     public static UserDB getInstance() {
         return instance;
     }
 
-    public User[] getUsers() { return users; }
+    public List<User> getUsers() { return users; }
 }
